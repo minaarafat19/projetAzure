669dc25c7c0dfd22834822d3455474dcd89964af
minaarafat19 <aminayoussoufarafat@gmail.com>
2022-06-17 22:55:57 +0200

    testing

---
+++ PortFolio/js/script.js
@@ -0,0 +1,3 @@
+var heading = document.getElementById("home")
+heading.setAttribute("class", "animate__animated animate__fadeInDown")
+heading = document.documentElement.style.setProperty('--animate-duration', '2s');
